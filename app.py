d0abd1f15bc05a7fbc3ccad7f05c8bb850633f85
Fabricio Vertamatti <favert@gmail.com>
2023-04-06 17:43:20 -0300

    Adding comments.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 
+# some test for retrieving response from REST API
 api_url = "http://jsonplaceholder.typicode.com/todos/1"
 response = requests.get(api_url)
 print(f'{response.json()}')
